841cd7a4fddc451b84be64e91b0c9f94b324452b
heganjie <heganjie@gmail.com>
2015-08-01 16:48:58 +0800

    大致完成抵押物类型的模式设计器

---
+++ Lip2p.Web/admin/loaner/mortgage_type_edit.aspx
@@ -0,0 +1,121 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="mortgage_type_edit.aspx.cs" Inherits="Lip2p.Web.admin.loaner.mortgage_type_edit" ValidateRequest="false" %>
+<%@ Import namespace="Lip2p.Common" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>抵押物类型编辑</title>
+<script type="text/javascript" src="../../scripts/jquery/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="../../scripts/jquery/Validform_v5.3.2_min.js"></script>
+<script type="text/javascript" src="../../scripts/lhgdialog/lhgdialog.js?skin=idialog"></script>
+<script type="text/javascript" src="../js/layout.js"></script>
+<link href="../skin/default/style.css" rel="stylesheet" type="text/css" />
+<style>
+.fieldSchemeContainer {
+    padding: 1em 0;
+}
+</style>
+<script type="text/javascript">
+    function beforeSubmit() {
+        var obj = {};
+        var $ids = $(".txtFieldId");
+        var $names = $(".txtFieldName");
+        $ids.each(function(index, elem) {
+            obj[elem.value] = $names[index].value;
+        });
+        $("#txtScheme").val(JSON.stringify(obj));
+        this.submit();
+    }
+    function cloneFieldScheme() {
+        $(".fieldSchemeContainer:last").clone().insertBefore("#btnAppendField").find("input.normal").val("");
+
+        $(".btnDeleteField").show().eq(0).hide();
+    }
+    function deleteFieldScheme(btn) {
+        $(btn).closest(".fieldSchemeContainer").remove();
+    }
+    function initFields(data) {
+        var obj = JSON.parse(data);
+        var firstTime = true;
+        for (var property in obj) {
+            if (obj.hasOwnProperty(property)) {
+                var container = firstTime ? $(".fieldSchemeContainer") : $(".fieldSchemeContainer:last").clone().insertBefore("#btnAppendField");
+                container.find(".txtFieldId").val(property);
+                container.find(".txtFieldName").val(obj[property]);
+                firstTime = false;
+            }
+        }
+    }
+
+    $(function () {
+        //初始化表单验证
+        $("#form1").initValidform();
+
+        initFields($("#txtScheme").val());
+        $(".btnDeleteField").show().eq(0).hide();
+    });
+</script>
+</head>
+
+<body class="mainbody">
+<form id="form1" runat="server" onsubmit="return beforeSubmit()">
+<!--导航栏-->
+<div class="location">
+  <a href="javascript:history.back(-1);" class="back"><i></i><span>返回列表页</span></a>
+  <a href="../center.aspx" class="home"><i></i><span>首页</span></a>
+  <i class="arrow"></i>
+  <a href="loaner_list.aspx"><span>抵押物类型列表</span></a>
+  <i class="arrow"></i>
+  <span>抵押物类型编辑</span>
+</div>
+<div class="line10"></div>
+<!--/导航栏-->
+
+<!--内容-->
+<div class="content-tab-wrap">
+  <div id="floatHead" class="content-tab">
+    <div class="content-tab-ul-wrap">
+      <ul>
+        <li><a href="javascript:;" onclick="tabs(this);" class="selected">编辑抵押物类型</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<div class="tab-content">
+  <dl>
+    <dt>名称</dt>
+    <dd><asp:TextBox ID="txtTypeName" runat="server" CssClass="input normal" datatype="/^\S+$/"></asp:TextBox><span class="Validform_checktip">*</span></dd>
+  </dl>
+  <dl>
+      <dt>字段</dt>
+      <dd>
+          <asp:HiddenField id="txtScheme" runat="server"/>
+          <div class="fieldSchemeContainer">
+              字段标识 <input class="txtFieldId input normal" datatype="/^\S+$/"/>
+              <br/>
+              字段名称 <input class="txtFieldName input normal" datatype="/^\S+$/"/>
+              <br/>
+              <input type="button" class="btnDeleteField" value="删除字段" onclick="deleteFieldScheme(this)"/>
+          </div>
+          <input type="button" id="btnAppendField" value="添加字段" onclick="cloneFieldScheme()"/>
+          <br/>
+          <span class="Validform_checktip"></span>
+      </dd>
+  </dl>
+</div>
+<!--/内容-->
+
+<!--工具栏-->
+<div class="page-footer">
+  <div class="btn-list">
+    <asp:Button ID="btnSubmit" runat="server" Text="提交保存" CssClass="btn" onclick="btnSubmit_Click" />
+    <input name="btnReturn" type="button" value="返回上一页" class="btn yellow" onclick="javascript:history.back(-1);" />
+  </div>
+  <div class="clear"></div>
+</div>
+<!--/工具栏-->
+</form>
+</body>
+</html>
---
+++ Lip2p.Web/admin/loaner/mortgage_type_edit.aspx.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Lip2p.Common;
+using Lip2p.Linq2SQL;
+
+namespace Lip2p.Web.admin.loaner
+{
+    public partial class mortgage_type_edit : UI.ManagePage
+    {
+        private string action = DTEnums.ActionEnum.Add.ToString(); //操作类型
+        private int id = 0;
+        Lip2pDataContext context = new Lip2pDataContext();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string _action = DTRequest.GetQueryString("action");
+            if (!string.IsNullOrEmpty(_action) && _action == DTEnums.ActionEnum.Edit.ToString())
+            {
+                action = DTEnums.ActionEnum.Edit.ToString();//修改类型
+                if (!int.TryParse(Request.QueryString["id"], out id))
+                {
+                    JscriptMsg("传输参数不正确！", "back", "Error");
+                    return;
+                }
+                var model = context.li_mortgage_types.FirstOrDefault(q => q.id == id);
+                if (model == null)
+                {
+                    JscriptMsg("记录不存在或已被删除！", "back", "Error");
+                    return;
+                }
+            }
+            if (!Page.IsPostBack)
+            {
+                ChkAdminLevel("loan_mortgage_types", DTEnums.ActionEnum.View.ToString()); //检查权限
+                if (action == DTEnums.ActionEnum.Edit.ToString()) //修改
+                {
+                    ShowInfo(id);
+                }
+            }
+        }
+
+        #region 赋值操作=================================
+        private void ShowInfo(int id)
+        {
+            var model = context.li_mortgage_types.First(q => q.id == id);
+            txtTypeName.Text = model.name;
+            txtScheme.Value = model.scheme;
+        }
+        #endregion
+
+        #region 增加操作=================================
+        private bool DoAdd()
+        {
+            var check = context.li_mortgage_types.FirstOrDefault(q => q.name == txtTypeName.Text.Trim()); //检测用户名是否重复
+            if (check != null)
+            {
+                JscriptMsg("类型名称重复！", "", "Error");
+                return false;
+            }
+            var model = new li_mortgage_types
+            {
+                name = txtTypeName.Text.Trim(),
+                scheme = txtScheme.Value,
+                last_update_time = DateTime.Now
+            };
+            context.li_mortgage_types.InsertOnSubmit(model);
+
+            try
+            {
+                context.SubmitChanges();
+                AddAdminLog(DTEnums.ActionEnum.Add.ToString(), "添加抵押物类型:" + model.name); //记录日志
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        #region 修改操作=================================
+        private bool DoEdit(int id)
+        {
+            var model = context.li_mortgage_types.First(q => q.id == id);
+
+            model.name = txtTypeName.Text.Trim();
+            model.scheme = txtScheme.Value;
+            model.last_update_time = DateTime.Now;
+
+            try
+            {
+                context.SubmitChanges();
+                AddAdminLog(DTEnums.ActionEnum.Edit.ToString(), "修改抵押物类型:" + model.name); //记录日志
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        //保存
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (action == DTEnums.ActionEnum.Edit.ToString()) //修改
+            {
+                ChkAdminLevel("loan_mortgage_types", DTEnums.ActionEnum.Edit.ToString()); //检查权限
+                if (!DoEdit(id))
+                {
+                    JscriptMsg("保存过程中发生错误！", "", "Error");
+                    return;
+                }
+                JscriptMsg("修改抵押物类型成功！", "mortgage_type_edit.aspx", "Success");
+            }
+            else //添加
+            {
+                ChkAdminLevel("loan_mortgage_types", DTEnums.ActionEnum.Add.ToString()); //检查权限
+                if (!DoAdd())
+                {
+                    JscriptMsg("保存过程中发生错误！", "", "Error");
+                    return;
+                }
+                JscriptMsg("添加抵押物类型成功！", "mortgage_type_edit.aspx", "Success");
+            }
+        }
+    }
+}
