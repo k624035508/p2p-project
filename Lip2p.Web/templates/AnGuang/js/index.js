ac0ded9466a0b96675de348ec9ea6bb12b8aa4f1
chilsa <316545823@qq.com>
2015-08-26 13:00:26 +0800

    首页返回顶部优化+标书右边小导航优化

--- Lip2p.Web/templates/AnGuang/js/index.js
+++ Lip2p.Web/templates/AnGuang/js/index.js
@@ -1,4 +1,4 @@
-require("bootstrap-webpack");
+﻿require("bootstrap-webpack");
 require("../css/head.css");
 require("../css/index.css");
 require("../css/invest-list.css");
@@ -18,4 +18,15 @@ $(function(){
     iconHover("#floating-qq", "qq-icon", "qqHover-icon");
     iconHover("#floating-cal", "cal-icon", "calHover-icon");
     iconHover("#floating-top", "top-icon", "topHover-icon");
+
+    //返回顶部浮窗隐藏与出现
+    $(window).scroll(function () {
+        var scrollTop = $(window).scrollTop();
+        console.log(scrollTop);
+        if (scrollTop >= 130) {
+            $("#floating-top-wrap").show();
+        } else {
+            $("#floating-top-wrap").hide();
+        }
+    });
 });
--- Lip2p.Web/templates/AnGuang/js/invest_detail.js
+++ Lip2p.Web/templates/AnGuang/js/invest_detail.js
@@ -3,3 +3,17 @@ require("bootstrap-webpack");
 require("../css/head.css");
 require("../css/invest_detail.less");
 require("../css/footer.css");
+
+$(function () {
+    $(window).scroll(function () {
+        var scrollTop = $(window).scrollTop();
+        var rightNav = $(".project-content-right ul");
+        if (scrollTop >= 560) {
+            rightNav.removeClass("notScroll");
+            rightNav.addClass("scrolled");
+        } else {
+            rightNav.removeClass("scrolled");
+            rightNav.addClass("notScroll");
+        }
+    });
+});
