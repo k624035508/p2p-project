c7217158c4d0f9ef628e9c2686a92128b4f95a6c
chilsa <316545823@qq.com>
2015-09-29 18:01:12 +0800

    会员中心调整

--- Agp2p.Web/templates/AnGuang/js/usercenter.js
+++ Agp2p.Web/templates/AnGuang/js/usercenter.js
@@ -12,8 +12,10 @@ $(function(){
     //加载交易明细内容
     var $mainContent = $("div.content-body");
     var basePath = $mainContent.data("templateskin");
+    var $nav = $(".outside-ul li");
 
     $("#tradeDetails").click(function(){
+        $nav.removeClass("nav-active");
         $(this).parent().addClass("nav-active");
         $mainContent.load(basePath + "/_tradeDetails.html", function () {
             //日期设置
@@ -40,16 +42,18 @@ $(function(){
 
     //加载我要充值内容
     $("#recharge").click(function(){
+        $nav.removeClass("nav-active");
         $(this).parent().addClass("nav-active");
-        $mainContent.load(basePath + "/_recharge.html");
+        $mainContent.load(basePath + "/_recharge.html",function(){
+            //充值选择银行
+            var $bank = $(".bank-select li");
+            $bank.click(function(){
+                $bank.find("img").remove();
+                var img = document.createElement("img");
+                img.src = basePath + "/imgs/usercenter/bank-icons/selected.png";
+                this.appendChild(img);
+            });
+        });
     });
 
-    //充值选择银行
-    var $bank = $(".bank-select li");
-    $bank.click(function(){
-        $bank.find("img").remove();
-        var img = document.createElement("img");
-        img.src = require("../imgs/usercenter/bank-icons/selected.png");
-        this.appendChild(img);
-    });
 });
