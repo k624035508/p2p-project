5074a1d5399eb2ae92593090d629bf619555e5d3
chilsa <316545823@qq.com>
2015-10-20 17:58:36 +0800

    会员中心-个人中心-安全中心-设置

--- Agp2p.Web/templates/AnGuang/containers/safe.jsx
+++ Agp2p.Web/templates/AnGuang/containers/safe.jsx
@@ -43,9 +43,12 @@ export default class SafeCenter extends React.Component {
 									<span className="list-tips">绑定邮箱，获取更多理财信息。</span>
 									<span className="pull-right"><a href="#">立即认证</a></span>
 								</div>
-								<div className="mail-setting-wrap">
-									<div className="cancel"><span className="th-setting">绑定邮箱</span><span className="glyphicon glyphicon-remove pull-right cancel-btn"></span></div>
-									<div className="mail-setting">
+								<div className="setting-wrap" id="email-setting">
+									<div className="cancel">
+										<span className="th-setting">绑定邮箱</span>
+										<span className="glyphicon glyphicon-remove pull-right cancel-btn"></span>
+									</div>
+									<div className="settings">
 										<div className="form-group">
 											<label for="email">您的邮箱：</label>
 											<input type="text" id="email" />
@@ -61,6 +64,34 @@ export default class SafeCenter extends React.Component {
 									<span className="list-tips">绑定手机，账户资金变动实时通知。</span>
 									<span className="pull-right"><a href="#">修改</a></span>
 								</div>
+								<div className="setting-wrap" id="phone-setting">
+									<div className="cancel">
+										<span className="th-setting">修改手机号</span>
+										<span className="glyphicon glyphicon-remove pull-right cancel-btn"></span>
+									</div>
+									<div className="settings">
+										<div className="oldPhone">
+											<span>原手机号码：</span>
+											<span className="phoneNum">13590609455</span>
+										</div>
+										<div className="form-group">
+											<label for="phone">新手机号码：</label>
+											<input type="text" id="phone" />
+										</div>
+										<div className="code-group img-code-wrap">
+											<label for="img-code">图形验证码：</label>
+											<input type="text" id="img-code" />
+											<span>图形验证码</span>
+											<a href="#" className="img-change">换一张</a>
+										</div>
+										<div className="code-group">
+											<label for="sms-code">短信验证码：</label>
+											<input type="text" id="sms-code" />
+											<a href="#" className="sms-code-btn">获取验证码</a>
+										</div>
+										<div className="btn-wrap"><a href="#">提 交</a></div>
+									</div>
+								</div>
 							</li>
 							<li>
 								<div className="list-cell">
@@ -68,6 +99,24 @@ export default class SafeCenter extends React.Component {
 									<span className="list-th">实名认证</span>
 									<span className="list-tips">保障账户资金安全，请使用本人身份证，提现时银行卡开户名与姓名一致。</span>
 									<span className="pull-right"><a href="#">立即认证</a></span>
+								</div>
+								<div className="setting-wrap" id="email-setting">
+									<div className="cancel">
+										<span className="th-setting">实名认证</span>
+										<span className="tips">为确保您的战虎安全，每个身份证号只能绑定一个安广融合账号</span>
+										<span className="glyphicon glyphicon-remove pull-right cancel-btn"></span>
+									</div>
+									<div className="settings">
+										<div className="form-group">
+											<label for="email">真实姓名：</label>
+											<input type="text" id="email" />
+										</div>
+										<div className="form-group">
+											<label for="personalID">身份证号：</label>
+											<input type="text" id="personalID" />
+										</div>
+										<div className="btn-wrap"><a href="#">提 交</a></div>
+									</div>
 								</div>
 							</li>
 							<li>
