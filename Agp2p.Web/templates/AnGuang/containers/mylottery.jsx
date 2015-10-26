bd5aab0f1fafa503643fd2a2583f189172fb8a8f
chilsa <316545823@qq.com>
2015-10-26 11:35:42 +0800

    个人中心-我的奖券-历史记录

--- Agp2p.Web/templates/AnGuang/containers/mylottery.jsx
+++ Agp2p.Web/templates/AnGuang/containers/mylottery.jsx
@@ -14,54 +14,67 @@ export default class MyLottery extends React.Component {
                         <span key={index}><a href="javascript:" className={this.state.selectedTabIndex == index ? "active" : null}
                             onClick={ ev => this.setState({ selectedTabIndex: index }) }>{s}</a></span>)}
                 </div>
-                <div className="lottery-list">
-                    <div className="lottery-usable">
-                        <div className="lottery-face">
-                            <p className="lottery-value">￥30</p>
-                            <p className="use-condition">投资满2000元可用</p>
-                            <p className="use-date">请于2015-10-30前使用</p>
+                <div className="lottery-content">
+                    <div className="lottery-list hidden">
+                        <div className="lottery-usable">
+                            <div className="lottery-face">
+                                <p className="lottery-value">￥30</p>
+                                <p className="use-condition">投资满2000元可用</p>
+                                <p className="use-date">请于2015-10-30前使用</p>
+                            </div>
+                            <div className="lottery-state">待使用</div>
                         </div>
-                        <div className="lottery-state">待使用</div>
+                        <div className="lottery-fail">
+                            <div className="lottery-face">
+                                <p className="lottery-value">￥30</p>
+                                <p className="use-condition">投资满2000元可用</p>
+                                <p className="use-date">请于2015-10-30前使用</p>
+                            </div>
+                            <div className="lottery-state">已失效</div>
+                        </div>
                     </div>
-                    <div className="lottery-usable">
-                        <div className="lottery-face">
-                            <p className="lottery-value">￥30</p>
-                            <p className="use-condition">投资满2000元可用</p>
-                            <p className="use-date">请于2015-10-30前使用</p>
-                        </div>
-                        <div className="lottery-state">待使用</div>
-                    </div>
-                    <div className="lottery-usable">
-                        <div className="lottery-face">
-                            <p className="lottery-value">￥30</p>
-                            <p className="use-condition">投资满2000元可用</p>
-                            <p className="use-date">请于2015-10-30前使用</p>
-                        </div>
-                        <div className="lottery-state">待使用</div>
-                    </div>
-                    <div className="lottery-fail">
-                        <div className="lottery-face">
-                            <p className="lottery-value">￥30</p>
-                            <p className="use-condition">投资满2000元可用</p>
-                            <p className="use-date">请于2015-10-30前使用</p>
-                        </div>
-                        <div className="lottery-state">已失效</div>
-                    </div>
-                    <div className="lottery-fail">
-                        <div className="lottery-face">
-                            <p className="lottery-value">￥30</p>
-                            <p className="use-condition">投资满2000元可用</p>
-                            <p className="use-date">请于2015-10-30前使用</p>
-                        </div>
-                        <div className="lottery-state">已失效</div>
-                    </div>
-                    <div className="lottery-fail">
-                        <div className="lottery-face">
-                            <p className="lottery-value">￥30</p>
-                            <p className="use-condition">投资满2000元可用</p>
-                            <p className="use-date">请于2015-10-30前使用</p>
-                        </div>
-                        <div className="lottery-state">已失效</div>
+                    <div className="lottery-history">
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th>红包金额</th>
+                                    <th>红包来源</th>
+                                    <th>使用规则</th>
+                                    <th>红包状态</th>
+                                    <th>有效期限</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>￥30</td>
+                                    <td>新手红包</td>
+                                    <td>投资金额满2000使用</td>
+                                    <td>待使用</td>
+                                    <td>2015-12-25</td>
+                                </tr>
+                                <tr>
+                                    <td>￥20</td>
+                                    <td>投资赠送</td>
+                                    <td>投资金额满1500使用</td>
+                                    <td>待使用</td>
+                                    <td>2015-12-30</td>
+                                </tr>
+                                <tr>
+                                    <td>￥30</td>
+                                    <td>新手红包</td>
+                                    <td>投资金额满2000使用</td>
+                                    <td>待使用</td>
+                                    <td>2015-12-25</td>
+                                </tr>
+                                <tr>
+                                    <td>￥20</td>
+                                    <td>投资赠送</td>
+                                    <td>投资金额满1500使用</td>
+                                    <td>待使用</td>
+                                    <td>2015-12-30</td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
                 <div className="use-rules-th"><span>使用规则</span></div>
