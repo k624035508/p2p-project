b046c81e0b290710954bc2de0076fa8598466251
heganjie <heganjie@gmail.com>
2015-11-02 17:42:36 +0800

    实现保存用户名

--- Agp2p.Web/templates/AnGuang/js/login.js
+++ Agp2p.Web/templates/AnGuang/js/login.js
@@ -6,32 +6,45 @@ import "../less/footerSmall.less";
 import $ from "jquery";
 
 $(function(){
-    $("#account").focus();
-    
-    $("#loginBtn").click(function(){
-        $.ajax({
-            type: "post",
-            url: "/tools/submit_ajax.ashx?action=user_login",
-            dataType: "json",
-            data: {
-                txtUserName: $("#account").val(),
-                txtPassword: $("#psw").val(),
-            },
-            success: function(data){
-                if(data.status == 1){
-                    location.href ="user/center/index.html";
-                } else {
-                    alert(data.msg);
-                }
-            },
-            error: function(data){
-                alert("操作超时，请重试");
-            }
-        });
-    });
+	$("#account").focus();
+
+	$("#loginBtn").click(function(){
+		$.ajax({
+			type: "post",
+			url: "/tools/submit_ajax.ashx?action=user_login",
+			dataType: "json",
+			data: {
+				txtUserName: $("#account").val(),
+				txtPassword: $("#psw").val(),
+			},
+			success: function(data){
+				if(data.status == 1){
+					location.href ="user/center/index.html";
+				} else {
+					alert(data.msg);
+				}
+			},
+			error: function(data){
+				alert("操作超时，请重试");
+			}
+		});
+		if (document.addEventListener) { //  >=ie9
+	        // 记住帐号
+	        if ($("input[type=checkbox]").is(":checked")) {
+	        	localStorage.setItem("webLogin_UserName", $("#account").val());
+	        } else {
+	        	localStorage.removeItem("webLogin_UserName");
+	        }
+	    }
+	});
     $("#psw").keyup(function (e) {
         if (e.keyCode == 13) {
             $("#loginBtn").click();
         }
     });
+
+    var userName = localStorage.getItem("webLogin_UserName");
+    if (userName) {
+    	$("#account").val(userName);
+    }
 });
