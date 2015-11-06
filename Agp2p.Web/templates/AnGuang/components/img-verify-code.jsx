409dff7643544dc24ba37972175fd4741720ec26
heganjie <heganjie@gmail.com>
2015-11-06 11:06:10 +0800

    完成 忘记密码 功能（界面未完善）

--- Agp2p.Web/templates/AnGuang/components/img-verify-code.jsx
+++ Agp2p.Web/templates/AnGuang/components/img-verify-code.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-class ImageVerifyCode extends React.Component {
+export default class ImageVerifyCode extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = { random: 0 };
 	}
 	render() {
-		return (<img src={`/tools/verify_code.ashx?r=${this.state.random}&seed=${this.props.seed || 0}`}/>);
+		return <img src={`/tools/verify_code.ashx?r=${this.state.random}&seed=${this.props.seed || 0}`}
+			onClick={ev => this.setState({random: Math.random()})}/>;
 	}
 }
-export default ImageVerifyCode;
