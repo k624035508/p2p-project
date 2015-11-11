60561789deb0f5b85ea24a227d0a5fd53f249e95
chilsa <316545823@qq.com>
2015-11-11 12:17:11 +0800

    关于我们-公司简介 图片放大浏览

--- Agp2p.Web/templates/AnGuang/js/aboutus.js
+++ Agp2p.Web/templates/AnGuang/js/aboutus.js
@@ -15,9 +15,18 @@ $(function(){
         $(this).parent().addClass("clicked");
     });
 
+    //加入我们 招聘列表开关样式
     var $office = $(".join-us-wrap .content-body .office ul li");
     $office.click(function(){
         $(this).find("i").toggleClass("glyphicon-menu-up");
         $(this).children(".office-detail").toggle();
+    });
+
+    //公司简介图片放大浏览
+    var $smallPic = $(".company-wrap .content-body .pic-wrap div");
+    var $bigPic = $("#picModal .modal-dialog .modal-content .modal-body img");
+    $smallPic.click(function(){
+        var picSrc = $(this).data("pic");
+        $bigPic.attr("src", picSrc);
     });
 });
