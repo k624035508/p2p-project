37cc8dbe14af9a04abb72bcf5c7f28fcec030915
heganjie <heganjie@gmail.com>
2015-11-17 17:17:27 +0800

    显示网站公告的同时显示发标公告

--- Agp2p.Web.UI/Page/article_list.cs
+++ Agp2p.Web.UI/Page/article_list.cs
@@ -9,6 +9,7 @@ namespace Agp2p.Web.UI.Page
     {
         protected int page;         //当前页码
         protected int category_id;  //类别ID
+        protected string category_ids;  //类别ID
         protected int totalcount;   //OUT数据总数
         protected string pagelist;  //分页页码
 
@@ -20,13 +21,20 @@ namespace Agp2p.Web.UI.Page
         {
             page = DTRequest.GetQueryInt("page", 1);
             category_id = DTRequest.GetQueryInt("category_id");
+            category_ids = DTRequest.GetQueryString("category_ids");
             if (category_id > 0) //如果ID获取到，将使用ID
             {
                 var bll = new BLL.article_category();
                 if (bll.Exists(category_id))
                     category = bll.GetModel(category_id);
             }
-          
+            else if (!string.IsNullOrWhiteSpace(category_ids))
+            {
+                var firstCategoryId = Convert.ToInt32(category_ids.Split(',')[0]);
+                var bll = new BLL.article_category();
+                if (bll.Exists(firstCategoryId))
+                    category = bll.GetModel(firstCategoryId);
+            }
         }
     }
 }
