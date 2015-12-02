f92e5d8cc8b5fea3a1b2bae928d18bce7176049a
heganjie <heganjie@gmail.com>
2015-12-02 16:52:42 +0800

    温馨提示对话框居中

--- Agp2p.Web/templates/AnGuang/components/tips_alert.js
+++ Agp2p.Web/templates/AnGuang/components/tips_alert.js
@@ -1,9 +1,12 @@
 import "../less/tips-alert.less"
 
 export default (msg, callback = null) => {
-    $("#tipsAlert .modal-body").text(msg);
-    $("#tipsAlert").modal();
-    if (callback) {
-        $("#tipsAlert button").off().on('click', callback);
-    }
+	$("#tipsAlert .modal-body").text(msg);
+	$("#tipsAlert").modal();
+	if (callback) {
+		$("#tipsAlert button").off().on('click', callback);
+	}
+
+	var offsetHeight = ($(window).height() - $("#tipsAlert .modal-content").height()) / 2;
+	$("#tipsAlert .modal-dialog").css("margin-top", offsetHeight + "px");
 }
