075880ecabff5b3648f1c25b810ef492486c15b0
chilsa <316545823@qq.com>
2015-12-03 15:20:48 +0800

    首页代码整理+图片更换

--- Agp2p.Web/templates/weChat/js/footer.js
+++ Agp2p.Web/templates/weChat/js/footer.js
@@ -3,16 +3,16 @@ import "../less/footer.less";
 export default function() {
     var key = typeof (forceFooterKey) === "undefined" ? window.location.href : forceFooterKey;
     if (key.indexOf("user") >= 0) {
-        $("#span_usercenter").attr("class", "footer-span-active icon-mine center-block");
-        $("#a_usercenter").attr("class", "txt-active");
+        $("#span_usercenter").attr("class", "active center-block");
+        $("#a_usercenter").attr("class", "active");
     } else if (key.indexOf("project") >= 0) {
-        $("#span_invest").attr("class", "footer-span-active icon-invest center-block");
-        $("#a_invest").attr("class", "txt-active");
+        $("#span_invest").attr("class", "active center-block");
+        $("#a_invest").attr("class", "active");
     } else if (key.indexOf("setting") >= 0) {
-        $("#span_setting").attr("class", "footer-span-active icon-settings center-block");
-        $("#a_setting").attr("class", "txt-active");
+        $("#span_setting").attr("class", "active center-block");
+        $("#a_setting").attr("class", "active");
     } else {
-        $("#span_index").attr("class", "footer-span-active icon-index center-block");
-        $("#a_index").attr("class", "txt-active");
+        $("#span_index").attr("class", "active center-block");
+        $("#a_index").attr("class", "active");
     }
 }
