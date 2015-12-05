7acf01d76a5eb4bf8be527d8cee35a476370957b
heganjie <heganjie@gmail.com>
2015-12-05 10:58:46 +0800

    完善帮助中心的 tab 高亮

--- Agp2p.Web/templates/AnGuang/js/help.js
+++ Agp2p.Web/templates/AnGuang/js/help.js
@@ -6,4 +6,17 @@ import "../less/footerSmall.less";
 $(function(){
     //弹出窗popover初始化
     $('[data-toggle="popover"]').popover();
+
+    var search = location.search || "?tab=0";
+    var match = search.match(/\?tab=(\d+)/);
+    var tabIndex = parseInt(match ? match[1] : "0");
+
+    if (tabIndex == 2) {
+    	$(".left-nav a.charge").closest("li").addClass("clicked");
+    	$(".charge-wrap").show();
+    } else if (categoryId != 0) {
+    } else if (tabIndex == 0) {
+    	$(".left-nav a.guide").closest("li").addClass("clicked");
+    	$(".guide-wrap").show();
+    }
 });
