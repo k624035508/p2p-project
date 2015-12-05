ec4ef9fd2f797b9cd25123a948fdba5b1172d820
sing173 <sing173@126.com>
2015-12-05 11:29:16 +0800

    企业添加股东信息字段

--- Agp2p.Web/admin/loaner/company_edit.aspx.designer.cs
+++ Agp2p.Web/admin/loaner/company_edit.aspx.designer.cs
@@ -112,6 +112,15 @@ namespace Agp2p.Web.admin.loaner {
         protected global::System.Web.UI.WebControls.TextBox txtBusinessStatus;
         
         /// <summary>
+        /// txtShareholder 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtShareholder;
+        
+        /// <summary>
         /// txtBusinessLawsuit 控件。
         /// </summary>
         /// <remarks>
