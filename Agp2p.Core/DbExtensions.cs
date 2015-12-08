053a5f6586a9acc01a18d900b55fe59692b72d96
heganjie <heganjie@gmail.com>
2015-12-08 17:02:17 +0800

    更新全局 timer 的逻辑

--- Agp2p.Core/DbExtensions.cs
+++ Agp2p.Core/DbExtensions.cs
@@ -9,7 +9,7 @@ namespace Agp2p.Core
 {
     public static class DbExtensions
     {
-        public static void AppendAdminLog(this Agp2pDataContext context, string actionType, string remark, int userId = 1, string userName = "admin")
+        public static void AppendAdminLog(this Agp2pDataContext context, string actionType, string remark, bool sysLog = false, int userId = 1, string userName = "admin")
         {
             var dtManagerLog = new dt_manager_log
             {
@@ -17,15 +17,15 @@ namespace Agp2p.Core
                 user_name = userName,
                 action_type = actionType,
                 remark = remark,
-                user_ip = DTRequest.GetIP(),
+                user_ip = sysLog ? "" : DTRequest.GetIP(),
                 add_time = DateTime.Now
             };
             context.dt_manager_log.InsertOnSubmit(dtManagerLog);
         }
 
-        public static void AppendAdminLogAndSave(this Agp2pDataContext context, string actionType, string remark, int userId = 1, string userName = "admin")
+        public static void AppendAdminLogAndSave(this Agp2pDataContext context, string actionType, string remark, bool sysLog = false, int userId = 1, string userName = "admin")
         {
-            context.AppendAdminLog(actionType, remark, userId, userName);
+            context.AppendAdminLog(actionType, remark, sysLog, userId, userName);
             context.SubmitChanges();
         }
     }
