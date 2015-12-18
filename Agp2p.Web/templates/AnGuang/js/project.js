6534096149f8c4b7fd4f6c6cc8aea4864fe52b87
chilsa <316545823@qq.com>
2015-12-18 15:45:00 +0800

    website 标书、我要理财页面 增加右侧功能浮窗

--- Agp2p.Web/templates/AnGuang/js/project.js
+++ Agp2p.Web/templates/AnGuang/js/project.js
@@ -32,6 +32,16 @@ $(function () {
 
     header.setHeaderHighlight(1);
 
+    // 返回顶部浮窗隐藏与出现
+    $(window).scroll(function () {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop >= 250) {
+            $("#floating-top-wrap").show();
+        } else {
+            $("#floating-top-wrap").hide();
+        }
+    });
+
     //弹出窗popover初始化
     $('[data-toggle="popover"]').popover();
 
