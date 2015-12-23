65f3eb162c67bf460d07fef7100bf0759b8ee176
chilsa <316545823@qq.com>
2015-12-23 15:19:17 +0800

    修正计算器无法计算的 bug

--- Agp2p.Web/templates/AnGuang/js/index.js
+++ Agp2p.Web/templates/AnGuang/js/index.js
@@ -5,30 +5,11 @@ import "../less/invest-cell.less";
 import "../less/footer.less";
 
 import header from "./header.js"
+window['$'] = $;
 
 $(function () {
     header.setHeaderHighlight(0);
 
-    // 返回顶部浮窗隐藏与出现
-    $(window).scroll(function () {
-        var scrollTop = $(window).scrollTop();
-        if (scrollTop >= 250) {
-            $("#floating-top-wrap").show();
-        } else {
-            $("#floating-top-wrap").hide();
-        }
-    });
-
     //计算器初始化
     $('[data-toggle="popover"]').popover();
-
-    window.calculate = function () {
-        var money = $("#money").val();
-        var rate = $("#rate").val();
-        var time = $("#time").val();
-        var profit = money * rate / 100 / 360 * time;
-        var forProfit = profit.toFixed(2);
-        $("#profit").html(forProfit);
-    }
-
 });
--- Agp2p.Web/templates/AnGuang/js/projects.js
+++ Agp2p.Web/templates/AnGuang/js/projects.js
@@ -6,19 +6,10 @@ import "../less/projects.less";
 import "../less/invest-cell.less";
 
 import header from "./header.js"
+window['$'] = $;
 
 $(function(){
     header.setHeaderHighlight(1);
-
-    // 返回顶部浮窗隐藏与出现
-    $(window).scroll(function () {
-        var scrollTop = $(window).scrollTop();
-        if (scrollTop >= 250) {
-            $("#floating-top-wrap").show();
-        } else {
-            $("#floating-top-wrap").hide();
-        }
-    });
 
     //弹出窗popover初始化
     $('[data-toggle="popover"]').popover();
