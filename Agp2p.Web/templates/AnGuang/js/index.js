21a9c4b7e27f95450f6e1f184262e1c975e37f5c
dengyijie <624035508@qq.com>
2016-03-17 16:01:04 +0800

    首页优化

--- Agp2p.Web/templates/AnGuang/js/index.js
+++ Agp2p.Web/templates/AnGuang/js/index.js
@@ -16,44 +16,46 @@ $(function () {
    //计算器初始化
     $('[data-toggle="popover"]').popover();
 
+   
     $(".project-content").hover(function(){
-        $(this).animate({paddingLeft:"20px",paddingRight:"10px"},300);
-    },function(){
-        $(this).animate({paddingLeft:"15px",paddingRight:"15px"},300);
-    });
 
-$(function(){  
+          $(this).stop().animate({paddingLeft:"20px",paddingRight:"10px"},300);
+    },function(){
+        
+           $(this).stop().animate({paddingLeft:"15px",paddingRight:"15px"},300);
+        });
+    
+
    
     var prog=$("div.progress-bar");       
     var changdu1=prog.parent().width();
     var changdu2=parseInt(prog.eq(0).html())/100;     
-    prog.eq(0).animate({width:changdu2*changdu1},500,"linear");    
+    prog.eq(0).width(changdu2*changdu1);    
    
-        $(window).stop(true).delay(500).scroll(function(){
+        $(window).scroll(function(){
            
             var windowtop = $(window).scrollTop();
-            if(windowtop>180 && windowtop<1000){        
+            if(windowtop>99 && windowtop<1040){        
    
                 for(var i=1;i<5;i++){
                     var changdu2=parseInt(prog.eq(i).html())/100;              
-                    prog.eq(i).stop(true).animate({width:changdu2*changdu1},500,"linear");        
+                    prog.eq(i).width(changdu2*changdu1);        
                 }      
 
             }
                 
-            if(windowtop>550){
+            if(windowtop>490){
     
                 for(var i=5;i<(prog.length);i++){
                     var changdu2=parseInt(prog.eq(i).html())/100;              
-                    prog.eq(i).stop(true).animate({width:changdu2*changdu1},500,"linear");        
+                    prog.eq(i).width(changdu2*changdu1);        
                 }
             }
          
             
         });
        
-       
-});
+
 
     //1.焦点图轮换
 $(".m-banner").slide({titCell:".hd li",mainCell:".bd ul",effect:"fold",autoPlay:true, trigger:"mouseover" })
