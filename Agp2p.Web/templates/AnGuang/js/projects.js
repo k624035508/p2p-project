1f5532a86371dd0cf8bf11ce199a03a3628895de
dengyijie <624035508@qq.com>
2016-03-17 16:59:36 +0800

    理财页面改动

--- Agp2p.Web/templates/AnGuang/js/projects.js
+++ Agp2p.Web/templates/AnGuang/js/projects.js
@@ -12,4 +12,26 @@ $(function(){
 
     //弹出窗popover初始化
     $('[data-toggle="popover"]').popover();
+
+    //进度条动画
+    var prog=$("div.progress-bar");       
+    var changdu1=prog.parent().width();
+    var changdu2=parseInt(prog.eq(0).html())/100;     
+    for(var i=0;i<9;i++){
+        var changdu2=parseInt(prog.eq(i).html())/100;              
+        prog.eq(i).width(changdu2*changdu1);        
+    }
+   
+    $(window).scroll(function(){
+       
+            var windowtop = $(window).scrollTop();     
+             if(windowtop>100){
+            for( var j=0;j<(prog.length);j++){
+                var changdu2=parseInt(prog.eq(j).html())/100;              
+                prog.eq(j).width(changdu2*changdu1);        
+            }
+        }
+         
+            
+    });
 });
