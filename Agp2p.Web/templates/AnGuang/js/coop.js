c651d1400f580919c835ddf572a8d2c92e433ef8
sing173 <sing173@126.com>
2016-03-29 11:33:24 +0800

    融资合作后台数据对接

--- Agp2p.Web/templates/AnGuang/js/coop.js
+++ Agp2p.Web/templates/AnGuang/js/coop.js
@@ -19,4 +19,30 @@ $(function () {
     },function(){
         $("a.teamwork").css("background","#1e94ff");
     });
+
+    $("#giving").click(function() {
+        $.ajax({
+            type: "post",
+            url: "/aspx/main/coop.aspx/Apply",
+            dataType: "json",
+            contentType: "application/json",
+            data: JSON.stringify({
+                userName: $("#accountname").val(),
+                mobile: $("#phone").val(),
+                type:"银票融资"
+            }),
+            success: function(data) {
+                var r = JSON.parse(data.d);
+                if(r.status == 1){
+                    //提交成功
+                    alert("提交申请成功!");
+                } else {
+                    alert(r.msg);
+                }
+            },
+            error: function(xhr, status, err){
+                alert("处理请求失败!请联系客服.");
+            }
+        });      
+    });
 });
