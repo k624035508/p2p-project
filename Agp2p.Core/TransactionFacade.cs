d038808e02621a28c6f68c2a6088a7cada50dfaa
heganjie <heganjie@gmail.com>
2016-04-03 09:36:51 +0800

    修正新手标回款逻辑

--- Agp2p.Core/TransactionFacade.cs
+++ Agp2p.Core/TransactionFacade.cs
@@ -1907,7 +1907,7 @@ namespace Agp2p.Core
                 Debug.Assert(Math.Round(claimRatio.Aggregate(0m, (sum, pair) => sum + pair.Value), 6) == 1, "债权比率之和不等于 1，会造成四舍五入结果异常");
 
             var rounded = claimRatio
-                .Where(pair => repaymentTask.only_repay_to == null || pair.Key.id == repaymentTask.only_repay_to) // 只对某投资者回款（新手标）
+                .Where(pair => repaymentTask.only_repay_to == null || pair.Key.userId == repaymentTask.only_repay_to) // 只对某投资者回款（新手标）
                 .Select(pair =>
                 {
                     var totalInterest = pair.Value*shouldRepayInterest;
