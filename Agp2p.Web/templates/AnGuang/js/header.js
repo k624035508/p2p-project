31c0c308f6108a554a258361a36fccebeda102d6
dengyijie <624035508@qq.com>
2016-04-05 15:49:38 +0800

    头部箭头优化

--- Agp2p.Web/templates/AnGuang/js/header.js
+++ Agp2p.Web/templates/AnGuang/js/header.js
@@ -1,10 +1,13 @@
 ﻿export default {
     setHeaderHighlight: function (index) {
         $("ul.in-header > li:nth(" + index + ") > a:first").addClass("nav-active");
+        $("ul.in-header > li:nth(" + index + ")").addClass("li-active");
     }
 };
 
 $(function(){
+
+
     $("#financial ul li").click(function(){
         var indexed=$("#financial ul li").index(this);
         $(".consume ul li").eq(indexed).addClass("bluecon").siblings().removeClass("bluecon");   
