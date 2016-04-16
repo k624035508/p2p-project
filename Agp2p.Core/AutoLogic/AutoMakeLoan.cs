c8a93b07c1a6fb72c4c72bdd4f1de4a1c0f466ce
sing173 <sing173@126.com>
2016-04-16 10:06:39 +0800

    排除aspx文件夹下的包含

---
+++ Agp2p.Core/AutoLogic/AutoMakeLoan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Agp2p.Common;
+using Agp2p.Core.Message;
+using Agp2p.Linq2SQL;
+
+namespace Agp2p.Core.AutoLogic
+{
+    /// <summary>
+    /// 每日自动放款（活期）
+    /// </summary>
+    class AutoMakeLoan
+    {
+        internal static void DoSubscribe()
+        {
+            MessageBus.Main.Subscribe<TimerMsg>(m => DoAutoMakeLoan(m.TimerType, m.OnTime)); // 每日定时检测逾期还款，设置为逾期状态
+        }
+
+        private static void DoAutoMakeLoan(TimerMsg.Type timerType, bool onTime)
+        {
+            if (timerType != TimerMsg.Type.AutoMakeLoanTimer) return;
+
+            var db = new Agp2pDataContext();
+            
+        }
+    }
+}
