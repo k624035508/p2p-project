7988b6ad6be70a41f3e4b544efc51c63174d8fe2
dengyijie <624035508@qq.com>
2016-04-18 15:54:52 +0800

    跳转返回

---
+++ Agp2p.Web/templates/AnGuang/js/success_return.js
@@ -0,0 +1,9 @@
+﻿import "bootstrap-webpack!./bootstrap.config.js"
+import "../less/head.less"
+import "../less/success_return.less"
+import "../less/footerSmall.less"
+
+$(function(){
+    //弹出窗popover初始化
+    $('[data-toggle="popover"]').popover();
+});
