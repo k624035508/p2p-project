40e2afbd67c1337f187c22373241ba94641449af
sing173 <sing173@126.com>
2016-05-13 16:28:51 +0800

    暂停充值

--- Agp2p.Web/templates/weChat/js/usercenter.js
+++ Agp2p.Web/templates/weChat/js/usercenter.js
@@ -11,9 +11,9 @@ $("html").css("font-size", fontSizeUnit);
 
 $(function() {
     //TODO 停止充值临时逻辑
-    //$("#reChargeBtn").click(function() {
-    //    alert("您暂时无法充值：安广融合平台正在切换第三方资金托管，具体全面开放请留意官网公告。");
-    //});
+    $("#reChargeBtn").click(function() {
+        alert("您暂时无法充值：安广融合平台正在切换第三方资金托管，具体全面开放请留意官网公告。");
+    });
 
     footerInit();
 });
