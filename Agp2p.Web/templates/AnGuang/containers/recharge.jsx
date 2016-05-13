235ca4f6cdd170318c7b083a23d9ca336dd7ede0
sing173 <sing173@126.com>
2016-05-13 15:24:24 +0800

    取消充值、提现限制

--- Agp2p.Web/templates/AnGuang/containers/recharge.jsx
+++ Agp2p.Web/templates/AnGuang/containers/recharge.jsx
@@ -17,13 +17,13 @@ class RechargePage extends React.Component {
 		if (this.props.groupName == "VIP会员") {
 			alert("您暂时无法充值：安广融合平台正在内部试运行中，具体全面开放请留意官网公告。");
 		}
-		alert("您暂时无法充值：安广融合平台正在切换第三方资金托管，具体全面开放请留意官网公告。");
+		//alert("您暂时无法充值：安广融合平台正在切换第三方资金托管，具体全面开放请留意官网公告。");
 	}
 	doCharge(ev) {
         //TODO 停止充值临时逻辑
-	    alert("您暂时无法充值：安广融合平台正在切换第三方资金托管，具体全面开放请留意官网公告。");
-	    ev.preventDefault();
-	    return;
+	    //alert("您暂时无法充值：安广融合平台正在切换第三方资金托管，具体全面开放请留意官网公告。");
+	    //ev.preventDefault();
+	    //return;
 		if (this.props.groupName == "VIP会员") {
 			alert("您暂时无法充值：安广融合平台正在内部试运行中，具体全面开放请留意官网公告。");
 			ev.preventDefault();
--- Agp2p.Web/templates/weChat/js/withdraw.js
+++ Agp2p.Web/templates/weChat/js/withdraw.js
@@ -45,8 +45,8 @@ $(function() {
     });
     $("#submit-btn").click(function() {
         //TODO 停止充值临时逻辑
-        alert("您暂时无法提现：安广融合平台正在切换第三方资金托管，具体全面开放请留意官网公告。");
-        return;
+        //alert("您暂时无法提现：安广融合平台正在切换第三方资金托管，具体全面开放请留意官网公告。");
+        //return;
 
         var withdrawVal = Number(txtHowmany.val());
         if (idleMoney < withdrawVal) {
