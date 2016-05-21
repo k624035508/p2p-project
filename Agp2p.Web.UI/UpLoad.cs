e35ea51c1258a512f8d99e2430334689a4a1dde8
dengyijie <624035508@qq.com>
2016-05-21 11:54:23 +0800

    广告优化、注册加载条

--- Agp2p.Web.UI/UpLoad.cs
+++ Agp2p.Web.UI/UpLoad.cs
@@ -60,7 +60,7 @@ namespace Agp2p.Web.UI
                 string fileExt = Utils.GetFileExt(postedFile.FileName); //文件扩展名，不含“.”
                 int fileSize = postedFile.ContentLength; //获得文件大小，以字节为单位
                 string fileName = postedFile.FileName.Substring(postedFile.FileName.LastIndexOf(@"\") + 1); //取得原文件名
-                string newFileName = fileName;  //随机生成新的文件名
+                string newFileName = Utils.GetRamCode() + "." + fileExt;  //随机生成新的文件名
                 string newThumbnailFileName = "thumb_" + newFileName; //随机生成缩略图文件名
                 string upLoadPath = GetUpLoadPath(); //上传目录相对路径
                 string fullUpLoadPath = Utils.GetMapPath(upLoadPath); //上传目录的物理路径
@@ -193,10 +193,10 @@ namespace Agp2p.Web.UI
             switch (this.siteConfig.filesave)
             {
                 case 1: //按年月每天一个文件夹
-                    path += DateTime.Now.ToString("yyyyMM");
+                    path += DateTime.Now.ToString("yyyyMMdd");
                     break;
                 default: //按年/月存入不同的文件夹
-                    path += DateTime.Now.ToString("yyyy") + "/" + DateTime.Now.ToString("MM");
+                    path += DateTime.Now.ToString("yyyyMM") + "/" + DateTime.Now.ToString("dd");
                     break;
             }
             return path + "/";
