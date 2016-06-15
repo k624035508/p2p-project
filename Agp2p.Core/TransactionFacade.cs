98bb462f9ac340222446544829a51980beb8e2e6
sing173 <sing173@126.com>
2016-06-15 09:05:50 +0800

    新手标按照360天计算利息

--- Agp2p.Core/TransactionFacade.cs
+++ Agp2p.Core/TransactionFacade.cs
@@ -1684,7 +1684,7 @@ namespace Agp2p.Core
             if (0 < proj.profit_rate)
                 return proj.profit_rate;
 
-            if (proj.dt_article_category.call_index == "ypb" || proj.dt_article_category.call_index == "ypl")
+            if (proj.dt_article_category.call_index == "ypb" || proj.dt_article_category.call_index == "ypl" || proj.IsNewbieProject2())
             {
                 var projectRepaymentTermSpanEnum = (Agp2pEnums.ProjectRepaymentTermSpanEnum)proj.repayment_term_span;
                 if (projectRepaymentTermSpanEnum != Agp2pEnums.ProjectRepaymentTermSpanEnum.Day)
