64d220c23b6a119587a47a0ae3918e63e4078fb7
dengyijie <624035508@qq.com>
2016-06-29 17:35:18 +0800

    问卷调查IE8兼容

--- Agp2p.Web/templates/AnGuang/js/questionnaire.js
+++ Agp2p.Web/templates/AnGuang/js/questionnaire.js
@@ -35,68 +35,70 @@ $(function() {
             $(".investAlert").css("z-index", "10");
         }
         
-    
+    //ie8兼容单选框样式
+    $("label").click(function() {
+        $(this).addClass("radioChecked");
+        $(this).parent().siblings().find("label").removeClass("radioChecked");
+    });
 
     //提交表格
         $("#questionnaireBtn").click(function() {
-            for (var j = 1 ; j <= 11; j++) {
                 var resualt = false;
-                var radios = document.getElementsByName("question" + j);
-                for (var i = 0; i < radios.length; i++) {
-                    if (radios[i].checked) {
-                        resualt = true;
-                    }
+                var radios = $(".question-list").length;
+            for (var i = 0; i < radios; i++) {
+                if ($(".question-list").eq(i).find("label").hasClass("radioChecked")) {
+                    resualt = true;
                 }
-                if (!resualt) {
-                    alert("问题"+j+"还没选择");
+                else {
+                    var indexq = i + 1;
+                    alert("问题" + indexq + "还没选择");
                     return resualt;
                 }
             }
 
-        var url = USER_CENTER_ASPX_PATH + "/AjaxSaveQuestionnaireResult";        
-        var radioLen = $("input[type='radio']:checked").length;
+
+            var url = USER_CENTER_ASPX_PATH + "/AjaxSaveQuestionnaireResult";        
+        var radioLen = $(".radioChecked").prev().length;
         var answers = new Array(radioLen);
         for (var i = 0; i < radioLen; i++) {
-            answers[i] = "\"" + $("input[type='radio']:checked").eq(i).val() + "\"";
-        }
-            
-        
+            answers[i] = "\"" + $(".radioChecked").prev().eq(i).val() + "\"";
+        }                  
         answers = "\[" + answers.join() + "\]";
-          $.ajax({
-              url: url,
-              type: "post",
-              dataType: "json",
-              contentType: "application/json",
-              data: JSON.stringify({
-                   questionnaireId : 1,
-                   result : answers
-              }),
-              success:function(data) {
-                  let score  = JSON.parse(data.d);
-                  if (score >= 31) {
-                      $(".typeMiddle").html("积极型");
-                      $(".typeP1").html("书山路，勤为径，");
-                      $(".typeP2").html("循序渐进齐头进。");
-                  }
-                  if (score >= 21 && score <= 30) {
-                      $(".typeMiddle").html("稳健型");
-                      $(".typeP1").html("黄叶落，白云扫，");
-                      $(".typeP2").html("稳健安全才最好。");
-                  }
-                  if ( score <= 20) {
-                      $(".typeMiddle").html("保守型");
-                      $(".typeP1").html("雷不动，打不动，");
-                      $(".typeP2").html("别人咋劝都没用。");
-                  }
-                  $(window).scrollTop(600);
-                  $(".investAlert").css("z-index", "10");
-                  
-              },
-              error: function (data) {
-                  alert("请登录");
-              }
-          });
-    });
+             $.ajax({
+                 url: url,
+                 type: "post",
+                 dataType: "json",
+                 contentType: "application/json",
+                 data: JSON.stringify({
+                      questionnaireId : 1,
+                      result : answers
+                 }),
+                 success:function(data) {
+                     let score  = JSON.parse(data.d);
+                     if (score >= 31) {
+                         $(".typeMiddle").html("积极型");
+                         $(".typeP1").html("书山路，勤为径，");
+                         $(".typeP2").html("循序渐进齐头进。");
+                     }
+                     if (score >= 21 && score <= 30) {
+                         $(".typeMiddle").html("稳健型");
+                         $(".typeP1").html("黄叶落，白云扫，");
+                         $(".typeP2").html("稳健安全才最好。");
+                     }
+                     if ( score <= 20) {
+                         $(".typeMiddle").html("保守型");
+                         $(".typeP1").html("雷不动，打不动，");
+                         $(".typeP2").html("别人咋劝都没用。");
+                     }
+                     $(window).scrollTop(600);
+                     $(".investAlert").css("z-index", "10");
+                     
+                 },
+                 error: function (data) {
+                     alert("请登录");
+                 }
+             }); 
+        });
 
    
 });
