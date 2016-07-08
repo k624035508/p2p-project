1561e9118eb25e7fe4dd4fb54938e291364d0fd5
dengyijie <624035508@qq.com>
2016-07-08 17:02:59 +0800

    积分商城实物详情页

---
+++ Agp2p.Web/templates/AnGuang/js/point_detail.js
@@ -0,0 +1,11 @@
+﻿import "bootstrap-webpack!./bootstrap.config.js";
+import "../less/head.less";
+import "../less/point_detail.less";
+import "../less/footerSmall.less";
+import header from "./header.js";
+window['jQuery'] = $;
+window['$'] = $;
+
+$(function() {
+    header.setHeaderHighlight(3); 
+})
--- Agp2p.Web/templates/AnGuang/webpack.config.js
+++ Agp2p.Web/templates/AnGuang/webpack.config.js
@@ -27,7 +27,8 @@ module.exports = {
         success_return: "./js/success_return.js",
         fail_return: "./js/fail_return.js",
         questionnaire: "./js/questionnaire.js",
-        point: "./js/point.js"
+        point: "./js/point.js",
+        point_detail: "./js/point_detail.js"
     },
     output: {
         path: path.join(__dirname, './build'),
@@ -39,7 +40,7 @@ module.exports = {
         new CommonsChunkPlugin("react.bundle.js", ["usercenter", "forgot_password", "article_show",
             "success_return","fail_return", "loan"]),
         new CommonsChunkPlugin("commons.bundle.js", ["react.bundle.js", "index", "login", "register", "project",
-            "projects", "aboutus", "safe_defence", "sitemap", "404", "help", "about_more", "coop", "success_return", "fail_return", "questionnaire", "point"]),
+            "projects", "aboutus", "safe_defence", "sitemap", "404", "help", "about_more", "coop", "success_return", "fail_return", "questionnaire", "point", "point_detail"]),
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery",
