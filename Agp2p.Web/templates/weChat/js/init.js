21dabf6762028932df3ef3fa93ef1e7ced6bc0bb
dengyijie <624035508@qq.com>
2016-09-28 18:02:21 +0800

    APP首页banner左右滑动

--- Agp2p.Web/templates/weChat/js/init.js
+++ Agp2p.Web/templates/weChat/js/init.js
@@ -37,4 +37,39 @@ var o = {
 		
 	}
 }
-$(function(){ o.init(); });
+$(function () { o.init(); });
+
+var page = 'banner-carousel';
+var mslide = 'slider';
+var mtitle = 'emtitle';
+arrdiv = 'arrdiv';
+
+var as = document.getElementById(page).getElementsByTagName('a');
+
+var tt = new TouchSlider({
+    id: mslide, 'auto': '-1', fx: 'ease-out', direction: 'left', speed: 600, timeout: 5000, 'before': function (index) {
+        var as = document.getElementById(this.page).getElementsByTagName('a');
+        as[this.p].className = '';
+        as[index].className = 'active';
+        this.p = index;
+        var txt = as[index].innerText;
+        $("#" + this.page).parent().find('.emtitle').text(txt);
+        var txturl = as[index].getAttribute('href');
+        var turl = txturl.split('#');
+        $("#" + this.page).parent().find('.go_btn').attr('href', turl[1]);
+    }
+});
+
+tt.page = page;
+tt.p = 0;
+//console.dir(tt); console.dir(tt.__proto__);
+for (var i = 0; i < as.length; i++) {
+    (function () {
+        var j = i;
+        as[j].tt = tt;
+        as[j].onclick = function () {
+            this.tt.slide(j);
+            return false;
+        }
+    })();
+}
